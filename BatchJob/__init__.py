76eb0b4c1ec1c73dc73a43068281aff63508e122
takuya_magoori <takuya_magori@mjs.co.jp>
2025-04-03 12:04:02 +0900

    add-except

--- BatchJob/__init__.py
+++ BatchJob/__init__.py
@@ -9,21 +9,26 @@ import os
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info(f'Python batchjob function processed a request.{req}')
-    
-    run_id = str(uuid.uuid4())  # GUID発行
-    logging.info('バッチ処理起動')
-    
-    # ストレージ接続文字列とキュー名（環境変数から取得）
-    conn_str = os.environ["AzureWebQueueJobsStorage"]
-    queue_name = "mago-batch-test-queue-poison"
+    try:
+        logging.info(f'Python batchjob function processed a request.{req}')
+        
+        run_id = str(uuid.uuid4())  # GUID発行
+        logging.info('バッチ処理起動')
+        
+        # ストレージ接続文字列とキュー名（環境変数から取得）
+        conn_str = os.environ["AzureWebQueueJobsStorage"]
+        queue_name = "mago-batch-test-queue-poison"
 
-    # キュークライアントを作成してメッセージを送信
-    queue_client = QueueClient.from_connection_string(conn_str, queue_name)
-    if not queue_client.exists():
-        queue_client.create_queue()
-    queue_client.send_message(json.dumps({ "run_id": run_id }))
+        # キュークライアントを作成してメッセージを送信
+        queue_client = QueueClient.from_connection_string(conn_str, queue_name)
+        if not queue_client.exists():
+            logging.info(f"[{run_id}] キューが存在しないため作成します: {queue_name};{conn_str}")
+            queue_client.create_queue()
+        queue_client.send_message(json.dumps({ "run_id": run_id }))
 
-    return func.HttpResponse(f"バッチ実行ID: {run_id}", status_code=202)
+        return func.HttpResponse(f"バッチ実行ID: {run_id}", status_code=202)
+    except Exception as e:
+        logging.exception(f"500エラー発生: {e}")
+        return func.HttpResponse("Internal Server Error", status_code=500)
 
 
