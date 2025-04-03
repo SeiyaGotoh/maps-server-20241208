a81d47d6e212c3fa384f569dd91ac79b75c9eab5
takuya_magoori <takuya_magori@mjs.co.jp>
2025-04-03 16:43:00 +0900

    型修正

--- QueueBatchProcessor/__init__.py
+++ QueueBatchProcessor/__init__.py
@@ -1,17 +1,20 @@
 import logging
 import json
 from shared.batch_logic import run_batch
+import azure.functions as func
 
-def main(msg: str):
+def main(msg: func.QueueMessage):
     logging.info("QueueTrigger 起動しました。msg内容:")
     logging.info(msg)
-    if msg.dequeue_count > 1:
-        logging.warning("このメッセージは2回目以降の処理です。スキップして削除します。")
-        return 
     try:
-        payload = json.loads(msg)
+        if msg.dequeue_count > 1:
+            logging.warning("このメッセージは2回目以降の処理です。スキップして削除します。")
+            return 
+        body = msg.get_body().decode("utf-8")
+        payload = json.loads(body)
         run_id = payload.get("run_id", "unknown")
         logging.info(f"[{run_id}] バッチ処理開始")
+        run_batch(run_id)
     except Exception as e:
         logging.error(f"メッセージ解析失敗: {e}")
         run_id = "invalid"
